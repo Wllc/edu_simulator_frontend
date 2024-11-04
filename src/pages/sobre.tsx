19c54eaa3a6fe9f97a736af148be621009abe60a
Wallace Gabriel <50117208+Wllc@users.noreply.github.com>
2024-11-04 20:56:55 -0300

    att de texto informativo

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -23,22 +23,22 @@ const Sobre: React.FC = () => {
     <div className="min-h-screen flex flex-col items-center bg-background">
       <div className="flex flex-col items-center mb-10 w-full max-w-4xl px-4">
         <div className="mt-10 bg-primary text-primary-foreground p-4 rounded-md">
-          <h2 className="text-xl font-semibold">O que é o OpenEduSim?</h2>
+          <h2 className="text-xl font-semibold">O QUE É O EDU SIMULATOR?</h2>
         </div>
 
         <InfoCard
-          title="OpenEduSim"
-          content="O OpenEduSim é uma plataforma de simulação de provas de vestibulares e concursos públicos. Aqui você pode fazer simulados de provas de vestibulares e concursos públicos de forma gratuita e participar do nosso ranking."
+          title="Edu Simulator"
+          content="O Edu Simulator é uma plataforma gratuita de simulação de provas do ENEM, onde você pode realizar simulados com questões de provas de 2009 a 2024. A ferramenta é projetada para auxiliar no preparo dos estudantes para o exame."
         />
 
         <InfoCard
           title="Objetivo"
-          content="O objetivo do OpenEduSim é ajudar estudantes a se prepararem para provas de vestibulares e concursos públicos. Aqui você pode fazer simulados de provas de vestibulares e concursos públicos de forma gratuita e participar do nosso ranking."
+          content="O objetivo do OpenEduSim é oferecer um ambiente de estudo eficaz para estudantes que desejam se preparar para o ENEM, ajudando-os a praticar e ganhar confiança para o exame."
         />
 
         <InfoCard
           title="Público-alvo"
-          content="O público-alvo deste projeto são os estudantes que estão se preparando para o ENEM"
+          content="O Edu Simulator é voltado para estudantes que estão se preparando para o ENEM e buscam aprimorar seus conhecimentos através de simulados realistas."
         />
 
         <InfoCard
